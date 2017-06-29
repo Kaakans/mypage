c0378cff31c967ed8df1db1a0c4831ae18cd967a
Jonathan Boellke <jonathan.boellke@nethouse.se>
2017-06-29 07:57:06 +0200

    added webpack image loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+					'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+				]
+			},
+			{
 				test: /\.js(x?)$/,
 				loader: 'babel-loader',
 				options: babelOptions
