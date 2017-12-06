c93a357daf602bb4c0652e348295f05011fd3481
Jonathan Boellke <jonathanboellke@gmail.com>
2017-12-06 09:29:22 +0100

    Review ordering and score

--- src/components/reviews.tsx
+++ src/components/reviews.tsx
@@ -21,24 +21,12 @@ export default class Reviews extends Component<any, any> {
                         In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
                     </p>
                 </div>
-            </div>
-            <div className="review-container">
-                <div className="review-image"></div>
-                <div className="review-content">
-                    <h1>Review title</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Phasellus enim leo, lobortis quis pretium in, viverra ut nibh. 
-                        Cras sollicitudin arcu dignissim dui dapibus, vel dapibus arcu mattis. 
-                        Nunc dapibus lacinia purus, non scelerisque odio congue ac. Etiam cursus tempus fringilla. 
-                        Maecenas et purus nibh. Duis orci augue, ullamcorper sed nulla ac, ultricies tincidunt massa. 
-                        Praesent erat nibh, euismod et vehicula non, eleifend at elit. 
-                        Vivamus aliquet tortor at condimentum vulputate. 
-                        Aliquam lobortis a dolor nec porta. 
-                        Proin in ipsum in leo euismod ultrices ut scelerisque leo. 
-                        Donec rutrum feugiat orci, id vestibulum est pellentesque ac. 
-                        In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
-                    </p>
+                <div className="review-score">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
                 </div>
             </div>
             <div className="review-container">
@@ -59,24 +47,12 @@ export default class Reviews extends Component<any, any> {
                         In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
                     </p>
                 </div>
-            </div>
-            <div className="review-container">
-                <div className="review-image"></div>
-                <div className="review-content">
-                    <h1>Review title</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Phasellus enim leo, lobortis quis pretium in, viverra ut nibh. 
-                        Cras sollicitudin arcu dignissim dui dapibus, vel dapibus arcu mattis. 
-                        Nunc dapibus lacinia purus, non scelerisque odio congue ac. Etiam cursus tempus fringilla. 
-                        Maecenas et purus nibh. Duis orci augue, ullamcorper sed nulla ac, ultricies tincidunt massa. 
-                        Praesent erat nibh, euismod et vehicula non, eleifend at elit. 
-                        Vivamus aliquet tortor at condimentum vulputate. 
-                        Aliquam lobortis a dolor nec porta. 
-                        Proin in ipsum in leo euismod ultrices ut scelerisque leo. 
-                        Donec rutrum feugiat orci, id vestibulum est pellentesque ac. 
-                        In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
-                    </p>
+                <div className="review-score">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
                 </div>
             </div>
             <div className="review-container">
@@ -97,6 +73,13 @@ export default class Reviews extends Component<any, any> {
                         In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
                     </p>
                 </div>
+                <div className="review-score">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                </div>
             </div>
             <div className="review-container">
                 <div className="review-image"></div>
@@ -115,6 +98,65 @@ export default class Reviews extends Component<any, any> {
                         Donec rutrum feugiat orci, id vestibulum est pellentesque ac. 
                         In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
                     </p>
+                </div>
+                <div className="review-score">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div className="review-container">
+                <div className="review-image"></div>
+                <div className="review-content">
+                    <h1>Review title</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus enim leo, lobortis quis pretium in, viverra ut nibh. 
+                        Cras sollicitudin arcu dignissim dui dapibus, vel dapibus arcu mattis. 
+                        Nunc dapibus lacinia purus, non scelerisque odio congue ac. Etiam cursus tempus fringilla. 
+                        Maecenas et purus nibh. Duis orci augue, ullamcorper sed nulla ac, ultricies tincidunt massa. 
+                        Praesent erat nibh, euismod et vehicula non, eleifend at elit. 
+                        Vivamus aliquet tortor at condimentum vulputate. 
+                        Aliquam lobortis a dolor nec porta. 
+                        Proin in ipsum in leo euismod ultrices ut scelerisque leo. 
+                        Donec rutrum feugiat orci, id vestibulum est pellentesque ac. 
+                        In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
+                    </p>
+                </div>
+                <div className="review-score">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div className="review-container">
+                <div className="review-image"></div>
+                <div className="review-content">
+                    <h1>Review title</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus enim leo, lobortis quis pretium in, viverra ut nibh. 
+                        Cras sollicitudin arcu dignissim dui dapibus, vel dapibus arcu mattis. 
+                        Nunc dapibus lacinia purus, non scelerisque odio congue ac. Etiam cursus tempus fringilla. 
+                        Maecenas et purus nibh. Duis orci augue, ullamcorper sed nulla ac, ultricies tincidunt massa. 
+                        Praesent erat nibh, euismod et vehicula non, eleifend at elit. 
+                        Vivamus aliquet tortor at condimentum vulputate. 
+                        Aliquam lobortis a dolor nec porta. 
+                        Proin in ipsum in leo euismod ultrices ut scelerisque leo. 
+                        Donec rutrum feugiat orci, id vestibulum est pellentesque ac. 
+                        In commodo vehicula eleifend. Phasellus quam sem, semper at auctor nec, volutpat et dolor.
+                    </p>
+                </div>
+                <div className="review-score">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
                 </div>
             </div>
         </div>;
